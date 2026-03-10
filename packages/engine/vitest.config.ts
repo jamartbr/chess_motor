2bf9b602aaf8e641e06fb3f225f33e7e6fae2dbe
Jaime Martinez <jamartbr@gmail.com>
2026-03-10 13:07:42 +0100

    minor test environment fixes

--- packages/engine/vitest.config.ts
+++ packages/engine/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals: true, // Allows using 'describe', 'it', 'expect' without importing them
+    globals: true,
     environment: 'node',
   },
 })
